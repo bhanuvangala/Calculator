c24f62cade9c7fe1a9876c24b91dbdc24423887a
bhanuvangala <bhanuvangala23568@gmail.com>
2024-09-04 19:27:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function backspace() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
